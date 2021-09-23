b3dc14017dac528bdf8b3ced6c18cba6872d9307
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-09-23 13:42:00 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = new express()
+
+app.use(express.static("public"))
+
+app.get('/hello', (request,response) => {
+    response.send("My new message")
+    console.log(request.ip)
+})
+
+app.get('/movies', (request, response) => {
+    const movie = {
+        name: "Ace Ventura Pet Detective",
+        rating: 10
+    }
+    response.send(movie)
+})
+
+app.listen(3000, () => {
+    console.log("server started")
+})
